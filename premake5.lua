92abe1cbd6b44a783149c69b35a57426bc9cec98
BELLIER Sacha <bellier.sacha2@gmail.com>
2023-11-28 16:52:38 +0100

    fix: premake

--- premake5.lua
+++ premake5.lua
@@ -1,17 +1,14 @@
 
-VULKAN_SDK = os.getenv("VULKAN_SDK")
-if (VULKAN_SDK == nil or VULKAN_SDK == '') then
-   VULKAN_SDK = os.getenv("VK_SDK_PATH")
-   if (VULKAN_SDK == nil or VULKAN_SDK == '') then
-      error("Coundn't load VulkanSDK, you may want to change the environement variable name or define it; syntax : VULKAN_SDK={path to the SDK directory}")
-   end
-end
-
-printf("Found VulkanSDK at path : %s", VULKAN_SDK)
+Solution.ProjectEnvVar["VULKAN_SDK"] = {
+   "VULKAN_SDK",
+   "VK_SDK_PATH"
+}
+ 
+VULKAN_SDK = Solution.GetEnvironementVariable("VULKAN_SDK");
 
 outputdir = Solution.Path.OutputDirectory
 
-Solution.ProjectsInfo.Includes["ProjectCore"] = {
+Solution.ProjectsInfo.IncludeDirs["Walnut"] = {
    "%{Solution.Projects.Walnut}/%{VULKAN_SDK}/Include",
    "%{Solution.Projects.Walnut}/vendor/glm",
    "%{Solution.Projects.Walnut}/vendor/imgui",
@@ -19,6 +16,8 @@ Solution.ProjectsInfo.Includes["ProjectCore"] = {
    "%{Solution.Projects.Walnut}/vendor/stb_image",
    "%{Solution.Projects.Walnut}/src"
 }
+
+Solution.ProjectsInfo.PlatformDefineName["Walnut"] = "WL"
 
 Solution.Projects["GLFW"] 	   = "vendor/GLFW"
 Solution.Projects["ImGui"] 	= "vendor/imgui"
@@ -39,23 +38,10 @@ project "Walnut"
 
    files { "src/**.h", "src/**.cpp" }
 
-   links
-   {
-       "ImGui",
-       "GLFW",
-       "%{VULKAN_SDK}/Lib/vulkan-1.lib"
+   links {
+      "ImGui",
+      "GLFW",
+      "%{VULKAN_SDK}/Lib/vulkan-1.lib"
    }
 
-	Solution.Project("Walnut")
-
-   filter "system:windows"
-      defines { "WL_PLATFORM_WINDOWS" }
-
-   filter "configurations:Debug"
-      defines { "WL_DEBUG" }
-
-   filter "configurations:Release"
-      defines { "WL_RELEASE" }
-
-   filter "configurations:Dist"
-      defines { "WL_DIST" }
+	Solution.IncludeProject("Walnut")
