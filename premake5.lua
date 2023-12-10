d30c70b20429769fb614760b036bbe8ca7f352c7
BELLIER Sacha <bellier.sacha2@gmail.com>
2023-12-10 18:40:25 +0100

    feat(PremakeUtilities): update

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,36 @@
 
-Solution.ProjectEnvVar["VULKAN_SDK"] = {
+VULKAN_SDK_ENVVAR = {
    "VULKAN_SDK",
    "VK_SDK_PATH"
 }
- 
-VULKAN_SDK = Solution.GetEnvironementVariable("VULKAN_SDK");
+
+VULKAN_SDK = Solution.GetEnvironementVariable("VULKAN_SDK", VULKAN_SDK_ENVVAR);
 
 outputdir = Solution.Path.OutputDirectory
 
-Solution.ProjectsInfo.IncludeDirs["Walnut"] = {
-   "%{Solution.Projects.Walnut}/%{VULKAN_SDK}/Include",
-   "%{Solution.Projects.Walnut}/vendor/glm",
-   "%{Solution.Projects.Walnut}/vendor/imgui",
-   "%{Solution.Projects.Walnut}/vendor/glfw/include",
-   "%{Solution.Projects.Walnut}/vendor/stb_image",
-   "%{Solution.Projects.Walnut}/src"
+Solution.Projects["Walnut"].PlatformDefineName = "WL"
+Solution.Projects["Walnut"].Type = "StaticLib"
+Solution.Projects["Walnut"].IncludeDirs = {
+   "%{Solution.Projects.Walnut.Path}/%{VULKAN_SDK}/Include",
+   "%{Solution.Projects.Walnut.Path}/vendor/glm",
+   "%{Solution.Projects.Walnut.Path}/vendor/imgui",
+   "%{Solution.Projects.Walnut.Path}/vendor/glfw/include",
+   "%{Solution.Projects.Walnut.Path}/vendor/stb_image",
+   "%{Solution.Projects.Walnut.Path}/src"
 }
 
-Solution.ProjectsInfo.PlatformDefineName["Walnut"] = "WL"
-
-Solution.Projects["GLFW"] 	   = "vendor/GLFW"
-Solution.Projects["ImGui"] 	= "vendor/imgui"
+Solution.AddProject("GLFW", "vendor/GLFW/")
+Solution.AddProject("ImGui", "vendor/imgui/")
+Solution.Projects["GLFW"].Type = "StaticLib"
+Solution.Projects["ImGui"].Type = "StaticLib"
 
 group "Dependencies"
-	include (Solution.Projects["GLFW"])
-	include (Solution.Projects["ImGui"])
+   Solution.PremakeIncludeProject("GLFW")
+   Solution.PremakeIncludeProject("ImGui")
 group ""
 
 project "Walnut"
-   kind "StaticLib"
+   kind 		   (Solution.Projects["Walnut"].Type)
    language "C++"
    cppdialect "C++20"
    staticruntime "off"
@@ -44,4 +46,4 @@ project "Walnut"
       "%{VULKAN_SDK}/Lib/vulkan-1.lib"
    }
 
-	Solution.IncludeProject("Walnut")
+	Solution.Project("Walnut")
